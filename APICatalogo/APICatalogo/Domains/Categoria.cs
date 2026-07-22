2ae0ab29027e26b31db36212f4d9953cd3be5fb0
z1nl0x <paulo.kreft@gmail.com>
2026-07-22 16:53:21 -0300

    Continuing the course

---
+++ APICatalogo/APICatalogo/Domains/Categoria.cs
@@ -0,0 +1,8 @@
+namespace APICatalogo.Domains;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
---
+++ API_Net10/API_Net10/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => 
+        options.SwaggerEndpoint("/openapi/v1.json", "Weather API"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
